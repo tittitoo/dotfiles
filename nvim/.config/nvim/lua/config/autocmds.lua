d45f06ca62df3f080ddb2376b86e6a0e0c872bfe
Thiha Aung <infowizard@gmail.com>
2024-08-28 17:50:21 +0800

    Change autocmds.lua to handle markdown file

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -4,23 +4,23 @@
 --
 
 -- disable completion on markdown files by default
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "gitcommit", "markdown" },
-  callback = function()
-    require("cmp").setup({ enabled = false })
-  end,
-})
-
--- Set textwidth to 80 and automatic line breaks for markdown files
 -- vim.api.nvim_create_autocmd("FileType", {
---   pattern = { "gitcommit", "markdown", "pandoc" },
+--   pattern = { "gitcommit", "markdown" },
 --   callback = function()
 --     require("cmp").setup({ enabled = false })
---     vim.opt_local.textwidth = 80
---     -- vim.opt_local.formatoptions:append("a")
---     vim.opt_local.colorcolumn = "80"
 --   end,
 -- })
+
+-- Set textwidth to 80 and automatic line breaks for markdown files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "gitcommit", "markdown", "pandoc" },
+  callback = function()
+    require("cmp").setup({ enabled = false })
+    vim.opt_local.textwidth = 80
+    -- vim.opt_local.formatoptions:append("a")
+    vim.opt_local.colorcolumn = "80"
+  end,
+})
 
 -- wrap and check for spell in text filetypes
 -- added to disable spelling
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,9 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-local opt = vim.opt
+-- local opt = vim.opt
 
 -- wrap / break
 
-opt.textwidth = 80
-opt.linebreak = true
+-- opt.textwidth = 80
+-- opt.linebreak = true
