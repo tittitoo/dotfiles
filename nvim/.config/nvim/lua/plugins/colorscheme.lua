84635088a3a2a517fb7fd1631f126cffce3a634c
Thiha Aung <infowizard@gmail.com>
2024-09-07 13:00:29 +0800

    Catppuccin theme added to lazyvim

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,15 @@
 return {
   -- Configure LazyVim to load desired colorscheme
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
       -- colorscheme = "retrobox",
-      colorscheme = "zellner",
+      -- colorscheme = "zellner",
+      colorscheme = "catppuccin-latte",
+      -- colorscheme = "catppuccin-mocha",
+      -- colorscheme = "catppuccin-macchiato",
+      -- colorscheme = "catppuccin-frappe",
     },
   },
 }
