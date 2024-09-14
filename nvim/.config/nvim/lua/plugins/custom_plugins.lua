231b9dbb3cc76cbff939fe16fbe3a89036b6f6ec
Thiha Aung <infowizard@gmail.com>
2024-09-14 12:59:54 +0800

    Install undotree plugin

--- nvim/.config/nvim/lua/plugins/custom_plugins.lua
+++ nvim/.config/nvim/lua/plugins/custom_plugins.lua
@@ -3,4 +3,12 @@
 return {
   { "folke/zen-mode.nvim", opts = {} },
   { "folke/twilight.nvim", opts = {} },
+  {
+    "jiaoshijie/undotree",
+    dependencies = "nvim-lua/plenary.nvim",
+    config = true,
+    keys = { -- load the plugin only when using it's keybinding:
+      { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
+    },
+  },
 }
