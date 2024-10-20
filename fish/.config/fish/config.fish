0853aa93d782e4b267b572ba155537c28c7dd13f
Thiha Aung <infowizard@gmail.com>
2024-10-20 10:15:12 +0800

    add config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,11 +6,29 @@ if test -f /Users/infowizard/anaconda3/bin/conda
 end
 # <<< conda initialize <<<
 
-
 export PATH="$PATH:$HOME/.local/bin"
+
+# Supress greeting message
+set fish_greeting
+
+# Homebrew setting
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Yazi
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
 
 # Atuin
 atuin init fish | source
 
 # Starship
 starship init fish | source
+
+# zoxide
+zoxide init fish | source
