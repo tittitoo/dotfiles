a18513b8e4b442cbfb145cf8dd5b6910bbc73a50
Thiha Aung <infowizard@gmail.com>
2024-10-21 18:08:43 +0800

    add fish plugins

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -11,8 +11,46 @@ export PATH="$PATH:$HOME/.local/bin:$HOME/.cargo/bin"
 # Supress greeting message
 set fish_greeting
 
+# Enable vi mode
+set fish_vi_key_bindings
+
+# fzf
+set -x FZF_DEFAULT_COMMAND 'fd --follow --exclude .git'
+set -x FZF_DEFAULT_OPTS '
+  --height=60%
+  # --tmux=left,70%
+  --layout=reverse
+  --info=inline
+  --preview="echo {}"
+  --preview-window=down:3:wrap
+  --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8
+  --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc
+  --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8
+  --color=selected-bg:#45475a
+  --border 
+  --multi
+  --bind "ctrl-u:preview-page-up,ctrl-d:preview-page-down"
+  --header "CTRL-T: Directories / CTRL-F: Files / CTRL-O: Open File / CTRL-Y: Download"
+  --bind "ctrl-t:change-prompt(Directories> )+reload(fd -t d)"
+  --bind "ctrl-f:change-prompt(Files> )+reload(fd -t f)"
+  --bind "ctrl-o:execute(open {})" 
+  --bind "ctrl-y:execute(cp {} ~/Downloads/)"
+  --bind "ctrl-/:change-preview-window(down|hidden|)"'
+set -x FZF_CTRL_T_OPTS '--walker-skip .git,node_modules,target,.obsidian'
+set -x FZF_ALT_C_OPTS '
+  --walker-skip .git,node_modules,target
+  --preview "tree -C {}"
+  --preview-window=bottom:50%:wrap
+  --bind "ctrl-u:preview-page-up,ctrl-d:preview-page-down"
+  --bind "ctrl-o:execute(open {})"'
+
+# fiz.fish
+# set fzf_preview_dir_cmd eza --all --color=always
+
 # Homebrew setting
-eval "$(/opt/homebrew/bin/brew shellenv)"
+# eval "$(/opt/homebrew/bin/brew shellenv)"
+eval "$(/usr/local/bin/brew shellenv)"
+# eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 # Yazi
 function y
