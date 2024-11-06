a075ddc43fe3836e9fbada51de14bbd2779ed5ba
Thiha Aung <infowizard@gmail.com>
2024-11-06 09:58:52 +0800

    add oil.nvim

--- nvim/.config/nvim/lua/plugins/custom_plugins.lua
+++ nvim/.config/nvim/lua/plugins/custom_plugins.lua
@@ -11,4 +11,12 @@ return {
       { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
     },
   },
+  -- oil-nvim
+  {
+    "stevearc/oil.nvim",
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  },
 }
