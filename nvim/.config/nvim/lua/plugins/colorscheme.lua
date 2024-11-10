e5bf6e126bb31757438315253b7e4648d6eb98d2
Thiha Aung <infowizard@gmail.com>
2024-11-10 10:21:52 +0800

    add kanagawa theme for nvim

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,19 @@
 return {
   -- Configure LazyVim to load desired colorscheme
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "rebelot/kanagawa.nvim", name = "kanagawa", priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
       -- colorscheme = "retrobox",
       -- colorscheme = "zellner",
       -- colorscheme = "catppuccin-latte",
-      colorscheme = "catppuccin-mocha",
+      -- colorscheme = "catppuccin-mocha",
       -- colorscheme = "catppuccin-macchiato",
       -- colorscheme = "catppuccin-frappe",
+      colorscheme = "kanagawa-wave",
+      -- colorscheme = "kanagawa-dragon",
+      -- colorscheme = "kanagawa-lotus",
     },
   },
 }
