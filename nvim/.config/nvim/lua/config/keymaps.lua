02b3f4a0e3310fd8bdb2be5438290b6f1b449061
Thiha Aung <infowizard@gmail.com>
2024-11-10 23:14:43 +0800

    add zenmode keyboard shortcut

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -16,3 +16,6 @@ vim.keymap.set("n", "-", "<cmd>Oil<CR>", { desc = "Open parent directory" })
 
 -- Split Window Below
 vim.keymap.set("n", '<leader>"', "<cmd>split<CR>", { noremap = false, silent = true, desc = "Split Window Below" })
+
+-- Zen Mode
+vim.keymap.set("n", "<leader>z", "<cmd>ZenMode<CR>", { desc = "ZenMode" })
