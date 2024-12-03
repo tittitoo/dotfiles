e6dcfbf31bce047156b4f4d28e5990610e6cdeeb
Thiha Aung <infowizard@gmail.com>
2024-12-03 10:16:40 +0800

    add markdownlint-cli2 config to ignore MD013

---
+++ nvim/.config/nvim/lua/plugins/lint.lua
@@ -0,0 +1,11 @@
+return {
+  "mfussenegger/nvim-lint",
+  optional = true,
+  opts = {
+    linters = {
+      ["markdownlint-cli2"] = {
+        args = { "--config", vim.fn.expand("~/.config/markdownlint-cli2/.markdownlint-cli2.yaml"), "--" },
+      },
+    },
+  },
+}
