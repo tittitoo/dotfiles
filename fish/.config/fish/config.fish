baab59141ed62c4174161cb8db906414833b4752
Thiha Aung <infowizard@gmail.com>
2024-12-09 21:44:09 +0800

    add python3.13 to path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -23,6 +23,9 @@ set -gx PATH $HOMEBREW_PREFIX/bin $PATH
 # ruby PATH
 set -gx PATH $HOMEBREW_PREFIX/opt/ruby/bin $PATH
 
+# python PATH
+set -gx PATH $HOMEBREW_PREFIX/bin/python3.13 $PATH
+
 # image.nvim
 set -gx DYLD_FALLBACK_LIBRARY_PATH $HOMEBREW_PREFIX/lib
 
