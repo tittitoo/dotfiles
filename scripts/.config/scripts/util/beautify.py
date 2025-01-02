151317ca983102a819066b2ff3d878c5206723ac
Thiha Aung <infowizard@gmail.com>
2025-01-02 20:41:33 +0800

    test beautify

--- scripts/.config/scripts/util/beautify.py
+++ scripts/.config/scripts/util/beautify.py
@@ -3,21 +3,43 @@
 import click
 
 TEMPLATE = [
-    "modec-commissioning-spares",
-    "modec-2yrs-spares",
-    "spl-cq-form",
+    "General",
+    "MODEC Commissioning Spares",
+    "MODEC 2YRS Spares",
+    "Seatrium CQ Form",
 ]
 
 
 @click.command()
 def beautify():
-    "Beautify excel file based on template"
+    "Beautify excel file based on template."
     click.echo("The following templates are available:")
-    for index, template in enumerate(TEMPLATE, start=1):
+    templates = {index: value for index, value in enumerate(TEMPLATE, start=1)}
+    for index, template in templates.items():
         click.echo(f"{index}: {template}")
-    template_number = click.prompt("Type the template number", type=int)
-    file = click.prompt("Choose the file name")
-    click.echo(f"{template_number}, {file}")
+    while True:
+        template_number = click.prompt("Type the template number or q to quit")
+        try:
+            if int(template_number) in templates.keys():
+                template_number = int(template_number)
+                break
+        except ValueError:
+            if template_number.lower() == "q":
+                return
+        click.echo("Not a valid value.")
+    file = click.prompt("The file name in the current directory")
+    click.echo(f"{templates[template_number]}, {file}")
+    match templates[template_number]:
+        case "General":
+            click.echo("General")
+        case "MODEC Commissioning Spares":
+            click.echo("MODEC Commissioning Spares")
+        case "MODEC 2YRS Spares":
+            click.echo("MODEC 2YRS Spares")
+        case "Seatrium CQ Form":
+            click.echo("Seatrium CQ Form")
+        case _:
+            click.echo("Invalid template")
 
 
 def test():
