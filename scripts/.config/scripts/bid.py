c3f3de0402348bb49d58a7abb03de68432e9b9b1
Thiha Aung <infowizard@gmail.com>
2025-03-20 11:40:33 +0800

    add AW to bid clean

--- scripts/.config/scripts/bid.py
+++ scripts/.config/scripts/bid.py
@@ -134,6 +134,7 @@ def require_rename(file_name: str, flag: bool = False) -> tuple[str, bool]:
     new_file_name = re.sub(r"(\]+)", "", new_file_name)
     new_file_name = re.sub(r"^(RE(_+|\s{1,}))", "", new_file_name, flags=re.IGNORECASE)
     new_file_name = re.sub(r"^(SV(_+|\s{1,}))", "", new_file_name, flags=re.IGNORECASE)
+    new_file_name = re.sub(r"^(AW(_+|\s{1,}))", "", new_file_name, flags=re.IGNORECASE)
     new_file_name = re.sub(r"^(FW(_+|\s{1,}))", "", new_file_name, flags=re.IGNORECASE)
     new_file_name = re.sub(r"^(FWD(_+|\s{1,}))", "", new_file_name, flags=re.IGNORECASE)
     new_file_name = re.sub(
