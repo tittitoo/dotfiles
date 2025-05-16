ca4675211ea4bcf094ff4fb999f0464c71ebd09a
Thiha Aung <infowizard@gmail.com>
2025-05-16 11:15:29 +0800

    silence warnings messages in pdfx

--- scripts/.config/scripts/util/pdfx.py
+++ scripts/.config/scripts/util/pdfx.py
@@ -2,7 +2,9 @@ import click
 import pypdf
 import os
 import re
+import logging
 from pathlib import Path
+from pypdf.errors import FileNotDecryptedError
 
 from reportlab.pdfgen import canvas
 
@@ -27,6 +29,10 @@ ACRONYMS = [
     "TQ",
 ]
 
+# Set logging level for pypdf so that warnings are not printed
+logger = logging.getLogger("pypdf")
+logger.setLevel(logging.ERROR)
+
 
 @click.command()
 @click.option("-o", "--outline", is_flag=True, help="Add outline to file from filename")
@@ -38,76 +44,76 @@ def combine_pdf(outline: bool, toc: bool):
     Combine pdf and output result pdf in the current folder.
     If the combined file already exists, it will remove it first and re-combine.
     """
-    directory = click.prompt(
-        "The command will merge all the pdf in the current directory: 'Enter' to accept, 'Ctrl+c' to abort",
-        default=Path.cwd(),
-        show_default=False,
-    )
-    directory = Path(directory)
-    filename = "00-Combined.pdf"
-    if os.path.exists(directory / filename):
-        os.remove(directory / filename)
+    if click.confirm(
+        "The command will merge all the pdf in the current directory", abort=True
+    ):
+        directory = Path.cwd()
+        filename = "00-Combined.pdf"
+        if os.path.exists(directory / filename):
+            os.remove(directory / filename)
 
-    # List pdf files
-    pdf_files = [
-        f for f in os.listdir(directory) if f.endswith("pdf") or f.endswith("PDF")
-    ]
+        # List pdf files
+        pdf_files = [
+            f for f in os.listdir(directory) if f.endswith("pdf") or f.endswith("PDF")
+        ]
 
-    # Sort the PDF files alphabetically
-    pdf_files.sort()
+        # Sort the PDF files alphabetically
+        pdf_files.sort()
 
-    # Creat a PdfMerger object
-    writer = pypdf.PdfWriter()
+        # Creat a PdfMerger object
+        writer = pypdf.PdfWriter()
 
-    encrypted_files: list[str] = []
-    # Add all the PDF files to the merger
-    try:
-        for pdf_file in pdf_files:
-            file_path = os.path.join(directory, pdf_file)
-            try:
-                # Read for side effect to see if it is encrypted
-                pypdf.PdfReader(file_path)
-                if outline:
-                    add_outline(Path(file_path))
-                writer.append(file_path)
-            except Exception:
-                encrypted_files.append(pdf_file)
-    except Exception:
-        pass
+        encrypted_files: list[str] = []
+        # Add all the PDF files to the merger
+        try:
+            for pdf_file in pdf_files:
+                file_path = os.path.join(directory, pdf_file)
+                try:
+                    # Read for side effect to see if it is encrypted
+                    pypdf.PdfReader(file_path)
+                    if outline:
+                        add_outline(Path(file_path))
+                    writer.append(file_path)
+                except FileNotDecryptedError:
+                    encrypted_files.append(pdf_file)
+                except Exception:
+                    pass
+        except Exception:
+            pass
 
-    # Write the ouput to a new PDF file
-    output_path = os.path.join(directory, filename)
-    try:
-        with open(output_path, "wb") as f:
-            writer.write(f)
-            if encrypted_files:
-                click.echo(
-                    "The following files are encrypted and not included in combined file."
-                )
-                for index, item in enumerate(encrypted_files):
+        # Write the ouput to a new PDF file
+        output_path = os.path.join(directory, filename)
+        try:
+            with open(output_path, "wb") as f:
+                writer.write(f)
+                if encrypted_files:
                     click.echo(
-                        f"{index + 1}: {item}"
-                    )  # Print the file name with index (item)
-            successful_pdf_files = list(set(pdf_files) - set(encrypted_files))
-            if successful_pdf_files:
-                click.echo(
-                    f"Combined following {len(successful_pdf_files)} files into '{filename}'"
-                )
-                successful_pdf_files.sort()
-                for index, item in enumerate(successful_pdf_files):
+                        "The following files are encrypted and not included in combined file."
+                    )
+                    for index, item in enumerate(encrypted_files):
+                        click.echo(
+                            f"{index + 1}: {item}"
+                        )  # Print the file name with index (item)
+                successful_pdf_files = list(set(pdf_files) - set(encrypted_files))
+                if successful_pdf_files:
                     click.echo(
-                        f"{index + 1}: {item}"
-                    )  # Print the file name with index (item)
-        if toc:
-            # TODO: Add code to add toc
-            click.echo("Still in implementation stage")
-            # toc_list = find_toc(Path(output_path))
-            # make_toc("00-toc.pdf", toc_list)
+                        f"Combined following {len(successful_pdf_files)} files into '{filename}'"
+                    )
+                    successful_pdf_files.sort()
+                    for index, item in enumerate(successful_pdf_files):
+                        click.echo(
+                            f"{index + 1}: {item}"
+                        )  # Print the file name with index (item)
+            if toc:
+                # TODO: Add code to add toc
+                click.echo("Still in implementation stage")
+                # toc_list = find_toc(Path(output_path))
+                # make_toc("00-toc.pdf", toc_list)
 
-    except Exception as e:
-        click.echo(f"Encountered this error {e}")
-    # Close the writer
-    writer.close()
+        except Exception as e:
+            click.echo(f"Encountered this error {e}")
+        # Close the writer
+        writer.close()
 
 
 def add_outline(file_path: Path):
