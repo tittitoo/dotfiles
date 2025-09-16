5ee80b27ba29cb69fef4d5b80d29fbe6b7c7d18e
Thiha Aung <infowizard@gmail.com>
2025-09-16 21:33:10 +0800

    wip kanagawa theme transparency not yet working

--- nvim/.config/nvim/lua/plugins/kanagawa-theme.lua
+++ nvim/.config/nvim/lua/plugins/kanagawa-theme.lua
@@ -1,39 +1,46 @@
 return {
-  {
-    "rebelot/kanagawa.nvim",
-    name = "kanagawa",
-    priority = 1000,
-    opts = {
-      transparent = true,
-      theme = "lotus",
+  "rebelot/kanagawa.nvim",
+  name = "kanagawa",
+  priority = 1000,
+  opts = {
+    transparent = true,
+    theme = "dragon",
+    background = {
+      dark = "dragon",
+      light = "lotus",
     },
-    -- This function is executed after the plugin and its options are set.
-    -- We now only pass the `opts` variable, since that's all we use.
-    config = function(opts)
-      require("kanagawa").setup(opts)
-      -- vim.cmd.colorscheme("kanagawa") -- change theme here for wave, dragon and lotus
-      -- vim.cmd.colorscheme("kanagawa-dragon") -- change theme here for wave, dragon and lotus
-      -- vim.cmd.colorscheme("kanagawa-wave") -- change theme here for wave, dragon and lotus
-      -- vim.cmd([[colorscheme kanagawa]])
-
-      -- Overriding the background for UI elements to be transparent
-      vim.cmd([[
-      hi Normal       guibg=NONE ctermbg=NONE
-      hi NormalFloat  guibg=NONE ctermbg=NONE
-      hi StatusLine   guibg=NONE ctermbg=NONE
-      hi StatusLineNC guibg=NONE ctermbg=NONE
-      hi WinBar       guibg=NONE ctermbg=NONE
-      hi WinBarNC     guibg=NONE ctermbg=NONE
-      hi TabLine      guibg=NONE ctermbg=NONE
-      hi TabLineFill  guibg=NONE ctermbg=NONE
-      hi Folded       guibg=NONE ctermbg=NONE
-    ]])
+    -- Explicitly set UI elements to have a transparent background.
+    colors = {
+      theme = {
+        all = {
+          ui = {
+            -- Set various background highlights to transparent.
+            bg_dim = "none",
+            bg_p1 = "none",
+            bg_p2 = "none",
+            bg_m3 = "none",
+            bg_gutter = "none",
+            bg_search = "none",
+            bg_visual = "none",
+          },
+        },
+      },
+    },
+    -- Use overrides to ensure transparency for floating windows and other elements.
+    overrides = function(colors)
+      local theme = colors.theme
+      return {
+        NormalFloat = { bg = "none" },
+        FloatBorder = { bg = "none" },
+        Folded = { bg = "none" },
+        -- Optional: Additional overrides for other plugins
+        TelescopeNormal = { bg = "none" },
+        -- You can add more here for plugins you use
+      }
     end,
   },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "kanagawa-wave",
-    },
-  },
+  config = function(opts)
+    require("kanagawa").setup(opts)
+    vim.cmd("colorscheme kanagawa")
+  end,
 }
