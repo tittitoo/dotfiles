d3c661b8d794993fd3d40f26f84cc1fe6243479c
Thiha Aung <infowizard@gmail.com>
2026-01-26 09:11:00 +0800

    remove conflicting undotree

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -8,9 +8,6 @@ vim.keymap.set("n", "<leader>m", '<cmd>r!open -a "Typora.app" "%"<cr>', { desc =
 -- Preview in Marked 2 App.
 vim.keymap.set("n", "<leader>M", '<cmd>r!open -a "Marked 2.app" "%"<cr>', { desc = "Open in Marked 2 app" })
 
--- Undotree
-vim.keymap.set("n", "<leader>uu", require("undotree").toggle, { noremap = true, silent = true, desc = "Undotree" })
-
 -- Toggle nvim-cmp
 vim.keymap.set("n", "<leader>uj", "<cmd>lua vim.g.cmptoggle = not vim.g.cmptoggle<CR>", { desc = "Toggle nvim-cmp" })
 
